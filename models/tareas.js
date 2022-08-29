b2a19967328b3be042379db38cb74583c4ed25e8
@francisco.peralta1 <francisco.peralta@umvel.com>
2022-08-29 17:57:45 -0500

    52. Crear y listar tareas.

--- models/tareas.js
+++ models/tareas.js
@@ -1,9 +1,19 @@
+const Tarea = require("./tarea");
 class Tareas {
   _listado = {};
 
   constructor() {
     this._listado = {};
   }
+
+  crearTarea(desc = "") {
+    const tarea = new Tarea(desc);
+
+    //Al mandar un id, estamos creando una nueva propiedad
+    this._listado[tarea.id] = tarea;
+  }
+
+  
 }
 
 module.exports = Tareas;
